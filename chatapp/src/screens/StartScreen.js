f8e3b048744129334e5f45ee0ab1266ead39707a
Zachary Tan <zdltan20@bu.edu>
2023-09-10 15:44:43 -0400

    login

---
+++ chatapp/src/screens/StartScreen.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Login</Header>
+      <Paragraph>
+        Welcome to our mood app!
+      </Paragraph>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        Login
+      </Button>
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate('RegisterScreen')}
+      >
+        Sign Up
+      </Button>
+      <Paragraph>
+        Made by Zachary Tan & Jakub Zolkos.
+      </Paragraph>
+    </Background>
+  )
+}
