dd142b30673bfb8e8e8c769ff0bee0e03de6e8ac
jakubzolkos <zolkos@bu.edu>
2023-09-11 09:29:50 -0400

    google authentication working

--- chatapp/firebaseConfig.js
+++ chatapp/firebaseConfig.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-
+import Constants from 'expo-constants'
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID,
+  apiKey: "AIzaSyBk-qINpI6XiNABTe_wbKfZykvQdvUWWlk",
+  authDomain: "chatapp-398518.firebaseapp.com",
+  projectId: "chatapp-398518",
+  storageBucket: "chatapp-398518.appspot.com",
+  messagingSenderId: "194340734719",
+  appId: "1:194340734719:web:7e54196b990c5d56932eca",
 };
 
 const app = initializeApp(firebaseConfig);
